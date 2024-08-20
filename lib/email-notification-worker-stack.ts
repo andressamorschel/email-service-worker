9b58b92418b58372376368b96bc3d0eedd08ac51
andressaurbano <andressa.urbano@hoamtechnology.com>
2024-08-20 20:02:45 -0300

    create email-notification-queue

--- lib/email-notification-worker-stack.ts
+++ lib/email-notification-worker-stack.ts
@@ -1,18 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from 'constructs';
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class EmailNotificationWorkerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "EmailNotificationWorker", {
+    var queue = new sqs.Queue(this, "email-notification-queue", {
+      queueName: "email-notification-queue",
+    });
+
+    const worker = new NodejsFunction(this, "EmailNotificationWorker", {
       runtime: Runtime.NODEJS_20_X,
       entry: "src/index.ts",
       bundling: {
         format: OutputFormat.ESM,
       }
     });
+
+    worker.addEventSource(new SqsEventSource(queue));
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export const handler = async () => {
-    return "Hello World!";
+import { Handler, SQSEvent } from "aws-lambda";
+
+export const handler: Handler = async (event: SQSEvent) => {
+    const messages = event.Records;
+  
+    for (let message of messages) {
+      console.log("Handling: ", message);
+    }
   };
